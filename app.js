6b35dee44ebb5113a98420215eb98deb8932bff0
smdedar <thededar@gmail.com>
2020-03-27 16:59:12 +0600

    country flag add

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var html = '';
+
+// fetch("https://corona.lmao.ninja/all")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       //html += '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text red darken-1"><span class="card-title truncate">' + 'Total' +'</span> <p></p> <p></p> <p></p> <p></p> <p class=" lime accent-4 black-text">Cases: ' + data.cases +'</p><p class=" red accent-4 accent-4 white-text">Deaths: ' + data.deaths + '</p><p class="cyan accent-3 black-text">Recovered: ' + data.recovered + '</p>  <p></p>  <p></p>  <p></p>  <p></p> </div> </div> </div>';
+       
+//     });
+
+
 var length;
   var url = 'https://corona.lmao.ninja/countries';
   fetch(url)
@@ -12,7 +23,7 @@ var length;
       for (var i = 0; i < length; i++) {
         if (data[i].country == "Bangladesh")
           html +=
-          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-1"><span class="card-title truncate">' + data[i].country +
+          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-1"><img  class="center-align" src="'+ data[i].countryInfo.flag+'" alt=""><span class="card-title truncate">' + data[i].country +
           '</span> <p class=" lime accent-4 black-text">Cases: ' + data[i].cases +
           '</p> <p class=" light-green accent-4 black-text">Today Cases: ' + data[i].todayCases +
           '</p> <p class=" red accent-4 accent-4 white-text">Deaths: ' + data[i].deaths +
@@ -26,7 +37,7 @@ var length;
 
 
         html +=
-          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-2"><span class="card-title truncate">' + data[i].country +
+          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-2"><img  class="center-align" src="'+ data[i].countryInfo.flag +'" alt=""><span class="card-title truncate">' + data[i].country +
           '</span> <p class=" lime accent-4 black-text">Cases: ' + data[i].cases +
           '</p> <p class=" light-green accent-4 black-text">Today Cases: ' + data[i].todayCases +
           '</p> <p class=" red accent-4 accent-4 white-text">Deaths: ' + data[i].deaths +
