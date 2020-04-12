229da5b2ae52553bd940731cae9074fd405b933c
smdedar <thededar@gmail.com>
2020-04-13 03:19:40 +0600

    update api url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var html = '';
 
 
 var length;
-  var url = 'https://corona.lmao.ninja/countries';
+  var url = 'https://corona.lmao.ninja/v2/countries?sort=cases';
   fetch(url)
     .then((response) => {
       return response.json();
@@ -23,7 +23,7 @@ var length;
       for (var i = 0; i < length; i++) {
         if (data[i].country == "Bangladesh")
           html +=
-          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-1"><img  class="center-align" src="'+ data[i].countryInfo.flag+'" alt=""><span class="card-title truncate">' + data[i].country +
+          '<div class="col s12 m4 l3"> <div class="card"> <div class="card-content white-text grey darken-3"><img  class="center-align" src="'+ data[i].countryInfo.flag+'" alt=""><span class="card-title truncate">' + data[i].country +
           '</span> <p class=" lime accent-4 black-text">Cases: ' + data[i].cases +
           '</p> <p class=" light-green accent-4 black-text">Today Cases: ' + data[i].todayCases +
           '</p> <p class=" red accent-4 accent-4 white-text">Deaths: ' + data[i].deaths +
